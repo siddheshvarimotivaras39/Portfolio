ebde7b319bdb4227bb8211463c4231f955b1fb6c
siddheshvarimotivaras39 <motivarassiddheshvari@gmail.com>
2025-05-20 20:50:41 +0530

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/Portfolio/" : "",
+  basePath: isProd ? "/Portfolio" : "",
+  output: "export",
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-images-1.medium.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
---
+++ utils/data/personal-data.js
@@ -0,0 +1,14 @@
+export const personalData = {
+  name: "Siddheshvari",
+  profile: 'profile.jpeg',
+  designation: "Software Developer",
+  description: "Full-stack Developer with expertise in React.js, Node.js, and modern JavaScript frameworks. Focused on delivering high-quality, responsive web applications on deadline",
+  email: 'mailto:motivarassiddheshvari@gmail.com',
+  phone: '+91 7016239372',
+  address: 'Porbandar, Gujarat, India',
+  github: 'https://github.com/siddheshvarimotivaras39',
+  linkedIn: 'https://www.linkedin.com/in/siddheshvari-motivaras-701056224',
+  leetcode: "https://leetcode.com/u/siddheshvari/",
+  devUsername: "siddheshvarimotivaras39",
+  resume: "https://drive.google.com/file/d/16ww3U8DP9MW4LXWe1RKiz9GO0MjOlhoR/view?usp=sharing"
+}
